7a8aeaea28b9f78c56426685f396907db5b254b8
Jethro <100454359+Whiplink@users.noreply.github.com>
2023-04-06 23:52:38 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,61 @@
+export const skills = [
+  {name:'HTML5', img:'./icons/html-1.svg'},
+  {name:'CSS3', img:'./icons/css-3.svg'},
+  {name:'JavaScript', img:'./icons/logo-javascript.svg'},
+  {name:'Python', img:'./icons/python-5.svg'},
+  {name:'ReactJs', img:'./icons/react-2.svg'},
+  {name:'NodeJs', img:'./icons/nodejs-icon.svg'},
+  {name:'ExpressJs', img:'./icons/expressjs-icon.svg'},
+  {name:'MongoDB', img:'./icons/mongodb-icon.svg'},
+  {name:'Github', img:'./icons/github-1.svg'},
+  {name:'Git', img:'./icons/git-scm-icon.svg'},
+  {name:'VSCode', img:'./icons/visual-studio-code-1.svg'},
+  {name:'Blender', img:'./icons/blender-2.svg'}
+]
+
+export const projects = [
+  {
+    name: 'Insure Landing Page',
+    img: './images/site1.jpg',
+    link: 'https://whiplink.github.io/Insure-Landing-Page/'
+  },
+  {
+    name: 'Loopstudios Landing Page',
+    img: './images/site2.jpg',
+    link: 'https://whiplink.github.io/Loopstudios-landing-page/'
+  },
+  {
+    name: 'News Hompage',
+    img: './images/site3.jpg',
+    link: 'https://newshomepage-whiplink.netlify.app/'
+  },
+  {
+    name: 'Job Listing',
+    img: './images/site4.jpg',
+    link: 'https://whiplink.github.io/Job-listing-with-filtering/'
+  },
+  {
+    name: 'Ecommerce Product Page',
+    img: './images/site5.jpg',
+    link: 'https://whiplink.github.io/Ecommerce-Product-Page/'
+  },
+  {
+    name: 'Social Media Dashboard',
+    img: './images/site6.jpg',
+    link: 'https://whiplink.github.io/Social-Media-Dashboard/index.html'
+  },
+  {
+    name: 'Advice Generator',
+    img: './images/site7.jpg',
+    link: 'https://whiplink.github.io/Advice-Generator/index.html'
+  }
+]
+
+export const renders = [
+  './images/exterior1.jpg',
+  './images/exterior2.jpg',
+  './images/exterior3.jpg',
+  './images/bathroom1.jpg',
+  './images/livingroom1.jpg',
+  './images/bedroom1.jpg'
+]
---
+++ script.js
@@ -0,0 +1,106 @@
+import { skills, projects, renders } from "./data.js"
+
+const bg = document.querySelector('.bg-gray')
+const mobileMenu = document.querySelector('.mobile-nav')
+const mobileMenuChilds = document.querySelectorAll('.mobile-nav > a')
+const menu = document.getElementById('menu')
+
+const show = () => {
+  bg.classList.toggle('show')
+  mobileMenu.classList.toggle('show')
+}
+
+bg.addEventListener('click', e =>{
+  if (e.target !== bg) return
+  show()
+})
+
+menu.addEventListener('click', () => {
+  show()
+})
+mobileMenuChilds.forEach(m => {
+  m.addEventListener('click', () => {
+    show()
+  })
+})
+
+const technologies = document.querySelector('.technologies')
+
+skills.forEach(s => {
+  const div = document.createElement('div')
+  const img = document.createElement('img')
+  const span = document.createElement('span')
+
+  div.classList.add('tech')
+  img.src = s.img
+  img.alt = s.name
+  span.textContent = s.name
+
+  div.append(img, span)
+  technologies.appendChild(div)
+})
+
+const projectsContainer = document.querySelector('.projects_container')
+
+projects.forEach(p => {
+  const imgContainer = document.createElement('div')
+  const img = document.createElement('img')
+  const hoverDiv = document.createElement('div')
+  const h1 = document.createElement('h1')
+  const linkBtn = document.createElement('a')
+
+  imgContainer.classList.add('project')
+  hoverDiv.classList.add('hover-div')
+
+  img.src = p.img
+  h1.textContent = p.name
+  linkBtn.textContent = 'Visit Site'
+  linkBtn.href = p.link
+
+  hoverDiv.append(h1, linkBtn)
+  imgContainer.append(img, hoverDiv)
+  projectsContainer.appendChild(imgContainer)
+})
+
+const rendersContainer = document.querySelector('.renders_container')
+const lightbox = document.querySelector('.lightbox')
+const lightboxImg = document.querySelector('.lightbox > img')
+
+renders.forEach((r, i) => {
+  const img = document.createElement('img')
+  img.src = r
+  img.alt = i
+  img.addEventListener('click', ()=>{
+    toggleLightbox(r)
+  })
+  rendersContainer.appendChild(img)
+})
+
+const toggleLightbox = r => {
+  lightbox.classList.toggle('show')
+  if (!r) {
+    lightboxImg.src = '/'
+    return
+  }
+  lightboxImg.src = r
+}
+
+lightbox.addEventListener('click', e=>{
+  toggleLightbox()
+})
+
+const sections = document.querySelectorAll('section')
+
+const observer = new IntersectionObserver(e=>{
+  e.forEach(x=>{
+    if(x.isIntersecting){
+      x.target.classList.add('fade')
+    } else {
+      x.target.classList.remove('fade')
+    }
+  })
+}, {rootMargin:'0px 0px -250px 0px'})
+
+sections.forEach(s=>{
+  observer.observe(s)
+})
